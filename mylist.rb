0af04defb84ec54df074371ef5bc7f11df14afac
John Murianki <mcjthiongo@gmail.com>
2023-08-24 19:57:02 +0300

    Add enumerable

--- mylist.rb
+++ mylist.rb
@@ -1,6 +1,18 @@
-require_relative 'myenymerable'
+require_relative 'myenumerable'
+
 class Mylist
-    def initialize(list)
+    include Myenumerable
+    def initialize(*list)
         @list = list
     end
+    def each(&block)
+        @list.each do |item|
+          block.call(item)
+        end
+      end
+      
 end
+list = Mylist.new(3,4,5,6,2,5,6)
+puts(list.all{|item| item>4})
+puts(list.any{|item| item>4})
+puts(list.filter{|item| item>4})
